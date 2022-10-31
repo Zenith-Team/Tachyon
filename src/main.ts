c45427249898ff9a66625f5e2b970b29de8bdf89
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-10-31 20:57:28 -0300

    fix -v flag

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ patch
 
 if (args.includes('-v') || args.includes('--version')) {
     try {
-        const { version } = JSON.parse(fs.readFileSync(path.join(fileURLToPath(import.meta.url), '..', 'package.json'), 'utf8'));
+        const { version } = JSON.parse(fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), '..', 'package.json'), 'utf8'));
         console.info(`Tachyon v${version}`);
     } catch {
         console.error('Failed to get version.');

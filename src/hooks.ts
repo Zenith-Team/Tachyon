3fbf3f41006be821d778d9d5d6200c6b8c27d9d3
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-10-30 21:22:14 -0300

    move everything to src folder

---
+++ src/hooks.ts
@@ -0,0 +1,166 @@
+﻿import { oFile, symbolMap } from './compile';
+import { SymbolSection } from 'rpxlib';
+import { hex, s32, u32 } from './utils';
+
+export interface HookYAML {
+    type: string;
+    addr: string;
+    instr: string; // Branch
+    func: string;  // Branch and Funcptr
+    count: s32;    // Multinop
+    data: string;  // Patch
+    value: string; // Return
+}
+
+export interface Patch {
+    address: u32;
+    data: string;
+}
+
+export abstract class Hook {
+    abstract get(): Patch;
+
+    protected source(): u32 {
+        return symbolMap.converter.convert(Number(this.address));
+    }
+
+    protected address: string = '';
+}
+
+export class PatchHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+
+        if (yaml.data.includes('0x')) {
+            console.warn('Warning: patch data is considered to be hex bytes, ignoring unnecessary "0x"');
+        }
+
+        this.data = yaml.data.replaceAll('0x', '');
+    }
+
+    public override get(): Patch {
+        return { address: this.source(), data: this.data };
+    }
+
+    data: string;
+}
+
+export class NopHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+    }
+
+    public override get(): Patch {
+        return { address: this.source(), data: '60000000' };
+    }
+
+}
+
+export class MultiNopHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+        this.count = yaml.count;
+    }
+
+    public override get(): Patch {
+        return { address: this.source(), data: '60000000'.repeat(this.count) };
+    }
+
+    count: u32;
+}
+
+export class ReturnValueHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+        this.returnValue = yaml.value!;
+    }
+
+    public override get(): Patch {
+        return {
+            address: this.source(),
+            data: '386000' + this.returnValue.padStart(2, '0') + '4E800020'
+        };
+    }
+
+    returnValue: string;
+};
+
+export class ReturnHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+    }
+
+    public override get(): Patch {
+        return { address: this.source(), data: '4E800020' };
+    }
+}
+
+export class BranchHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+        this.instr = yaml.instr;
+        this.func = yaml.func;
+    }
+
+    public override get(): Patch {
+        let instr: u32 = 0;
+        let target: u32;
+
+        try {
+            const symtab = oFile.sections.find(section => section.name === '.symtab') as SymbolSection | undefined;
+            if (symtab === undefined) throw null;
+            const value = symtab.symbols.find(sym => sym.name === this.func)?.value;
+            if (value === undefined) throw null;
+            target = +value;
+        } catch (err) {
+            if (err !== null) throw err;
+            target = symbolMap.getSymbol(this.func).address!;
+        }
+
+        instr = ((target) - this.source()) & 0x03FFFFFC;
+
+        if      (this.instr === 'b')  instr |= 0x48000000;
+        else if (this.instr === 'bl') instr |= 0x48000001;
+        else console.warn('Unknown branch instruction:', this.instr);
+
+        return {
+            address: this.source(),
+            data: hex(instr).slice(0, 8)
+        };
+    }
+
+    instr: string;
+    func: string;
+}
+
+export class FuncptrHook extends Hook {
+    constructor(yaml: HookYAML) {
+        super();
+        this.address = yaml.addr;
+        this.func = yaml.func;
+    }
+
+    public override get(): Patch {
+        let target: u32;
+
+        try {
+            const symtab = oFile.sections.find(section => section.name === '.symtab')! as SymbolSection;
+            target = symtab.symbols.find(sym => sym.name === this.func)!.value.valueOf();
+        } catch {
+            target = symbolMap.getSymbol(this.func).address!;
+        }
+
+        return {
+            address: this.source(),
+            data: hex(target)
+        };
+    }
+
+    func: string;
+}
---
+++ src/patchrpx.ts
@@ -0,0 +1,168 @@
+import {
+    RPL, Util, Section, NoBitsSection, RelocationSection, StringSection, SymbolSection, LoadBaseAddress
+} from 'rpxlib';
+import { u32, s32, hex, abort } from './utils';
+import { Patch } from './hooks';
+
+export function patchRPX(sourceRPX: RPL, destRPX: RPL, patches: Patch[], brand: string, addrs: { syms: u32, text: u32, data: u32 }) {
+    interface SectionMap {
+        text: Section, rodata: Section, data: Section, bss: NoBitsSection,
+        symtab: SymbolSection, strtab: StringSection,
+        relatext: RelocationSection,
+        relarodata: RelocationSection,
+        reladata: RelocationSection
+    }
+
+    const rpxSections = {} as SectionMap;
+    for (const section of destRPX.sections) {
+        switch (section.name) {
+            case '.text': rpxSections.text = section; break;
+            case '.rodata': rpxSections.rodata = section; break;
+            case '.data': rpxSections.data = section; break;
+            case '.bss': rpxSections.bss = section as NoBitsSection; break;
+            case '.symtab': rpxSections.symtab = section as SymbolSection; break;
+            case '.strtab': rpxSections.strtab = section as StringSection; break;
+            case '.rela.text': rpxSections.relatext = section as RelocationSection; break;
+            case '.rela.rodata': rpxSections.relarodata = section as RelocationSection; break;
+            case '.rela.data': rpxSections.reladata = section as RelocationSection; break;
+        }
+    }
+    const sourceRPXSections = {} as Partial<SectionMap> & { text: Section };
+    for (const section of sourceRPX.sections) {
+        switch (section.name) {
+            case '.text': sourceRPXSections.text = section; break;
+            case '.rodata': sourceRPXSections.rodata = section; break;
+            case '.data': sourceRPXSections.data = section; break;
+            case '.bss': sourceRPXSections.bss = section as NoBitsSection; break;
+            case '.symtab': sourceRPXSections.symtab = section as SymbolSection; break;
+            case '.strtab': sourceRPXSections.strtab = section as StringSection; break;
+            case '.rela.text': sourceRPXSections.relatext = section as RelocationSection; break;
+            case '.rela.rodata': sourceRPXSections.relarodata = section as RelocationSection; break;
+            case '.rela.data': sourceRPXSections.reladata = section as RelocationSection; break;
+        }
+    }
+
+    sourceRPXSections.text.nameOffset = destRPX.shstrSection.strings.add(`.text.${brand}`);
+    sourceRPXSections.text.flags = rpxSections.text.flags;
+    destRPX.pushSection(sourceRPXSections.text);
+
+    if (sourceRPXSections.rodata) {
+        sourceRPXSections.rodata.nameOffset = destRPX.shstrSection.strings.add(`.rodata.${brand}`);
+        sourceRPXSections.rodata.flags = rpxSections.rodata.flags;
+        destRPX.pushSection(sourceRPXSections.rodata);
+    }
+    if (sourceRPXSections.data) {
+        sourceRPXSections.data.nameOffset = destRPX.shstrSection.strings.add(`.data.${brand}`);
+        sourceRPXSections.data.flags = rpxSections.data.flags;
+        destRPX.pushSection(sourceRPXSections.data);
+    }
+    if (sourceRPXSections.bss) {
+        sourceRPXSections.bss.nameOffset = destRPX.shstrSection.strings.add(`.bss.${brand}`);
+        sourceRPXSections.bss.flags = rpxSections.bss.flags;
+        destRPX.pushSection(sourceRPXSections.bss);
+    }
+    if (sourceRPXSections.relatext) {
+        sourceRPXSections.relatext.nameOffset = destRPX.shstrSection.strings.add(`.rela.text.${brand}`);
+        sourceRPXSections.relatext.flags = rpxSections.relatext.flags;
+        destRPX.pushSection(sourceRPXSections.relatext);
+    }
+    if (sourceRPXSections.relarodata) {
+        sourceRPXSections.relarodata.nameOffset = destRPX.shstrSection.strings.add(`.rela.rodata.${brand}`);
+        sourceRPXSections.relarodata.flags = rpxSections.relarodata.flags;
+        destRPX.pushSection(sourceRPXSections.relarodata);
+    }
+    if (sourceRPXSections.reladata) {
+        sourceRPXSections.reladata.nameOffset = destRPX.shstrSection.strings.add(`.rela.data.${brand}`);
+        sourceRPXSections.reladata.flags = rpxSections.reladata.flags;
+        destRPX.pushSection(sourceRPXSections.reladata);
+    }
+    if (sourceRPXSections.symtab) {
+        sourceRPXSections.symtab.nameOffset = destRPX.shstrSection.strings.add(`.symtab.${brand}`);
+        sourceRPXSections.symtab.flags = rpxSections.symtab.flags;
+        sourceRPXSections.symtab.addr = Util.roundUp(addrs.syms, +sourceRPXSections.symtab.addrAlign);
+        destRPX.pushSection(sourceRPXSections.symtab);
+    }
+    if (sourceRPXSections.strtab) {
+        sourceRPXSections.strtab.nameOffset = destRPX.shstrSection.strings.add(`.strtab.${brand}`);
+        sourceRPXSections.strtab.flags = rpxSections.strtab.flags;
+        sourceRPXSections.strtab.addr = Util.roundUp(
+            addrs.syms + (sourceRPXSections.symtab ? +sourceRPXSections.symtab.size : 0),
+            +sourceRPXSections.strtab.addrAlign
+        );
+        destRPX.pushSection(sourceRPXSections.strtab);
+    }
+    if (sourceRPXSections.symtab && sourceRPXSections.strtab) {
+        sourceRPXSections.symtab.link = sourceRPXSections.strtab.index;
+    }
+    destRPX.crcSection.nameOffset = destRPX.shstrSection.strings.add(`.rplcrcs`);
+    destRPX.fileinfoSection.nameOffset = destRPX.shstrSection.strings.add(`.rplfileinfo`);
+
+    for (const patch of patches) {
+        let targetSection: Section;
+        let targetRelocSection: RelocationSection;
+
+        const address: u32 = patch.address;
+        const data: string = patch.data;
+
+        if (rpxSections.text.addr <= address && address < addrs.text) {
+            targetSection = rpxSections.text;
+            targetRelocSection = rpxSections.relatext;
+        } else if (Math.min(+rpxSections.rodata.addr, +rpxSections.data.addr) <= address && address < addrs.data) {
+            if (rpxSections.rodata.addr <= address && address < (<number>rpxSections.rodata.addr + <number>rpxSections.rodata.size)) {
+                targetSection = rpxSections.rodata;
+                targetRelocSection = rpxSections.relarodata;
+            } else if (rpxSections.data.addr <= address && address < (<number>rpxSections.data.addr + <number>rpxSections.data.size)) {
+                targetSection = rpxSections.data;
+                targetRelocSection = rpxSections.reladata;
+            } else {
+                console.warn(
+                    `Patch of data "${data}" at address 0x${
+                        hex(address)
+                    } is within the data sections bounds but outside all known patchable data sections (.data & .rodata)`
+                );
+                continue;
+            }
+        } else {
+            console.warn(`Patch of data "${data}" at address 0x${hex(address)} is out of bounds.`);
+            continue;
+        }
+
+        const relocSize = +targetRelocSection.entSize;
+        const relocData = targetRelocSection.data;
+        let offsets: number[] = [];
+        let low: s32 = 0;
+        let high: s32 = relocData.byteLength / (<number>targetRelocSection.entSize) - 1;
+        while (low <= high) {
+            const mid: s32 = ~~((low + high) / 2);
+            const at: number = mid * relocSize;
+            const val: u32 = (relocData[at] << 24 | relocData[at+1] << 16 | relocData[at+2] << 8 | relocData[at+3]) >>> 0;
+            if (val < address) low = mid + 1;
+            else if (val > address + 4) high = mid - 1;
+            else {
+                offsets.push(mid * relocSize);
+                break;
+            }
+        }
+        let start = 0;
+        const sink = new Util.ArrayBufferSink();
+        sink.start({ asUint8Array: true });
+        for (const offset of offsets.sort((a, b) => a - b)) {
+            sink.write(relocData.subarray(start, offset));
+            start = offset + 12;
+        }
+        sink.write(relocData.subarray(start));
+        targetRelocSection.data = sink.end() as Uint8Array;
+
+        if (data.length % 2) abort(`Data of patch at ${address} of section ${targetSection.name} is not byte aligned: ${data}`);
+
+        const dataBytes = Buffer.from(data, 'hex');
+        for (let i = 0; i < dataBytes.byteLength; i++) {
+            targetSection.data![(address - <number>targetSection.addr) + i] = dataBytes[i];
+        }
+    }
+
+    destRPX.shstrSection.addr = destRPX.addressRanges.free.find(([start]) => start >= LoadBaseAddress)![0];
+    destRPX.fileinfoSection.adjustFileInfoSizes();
+    // CEMU subtracts 0x90 from this value for some random reason (?)
+    (<number>destRPX.fileinfoSection.fileinfo.loadSize) += 0x90;
+}

529b39ef51869dc5ac37f58f93648f71bb29ef6d
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-03-14 06:19:20 -0300

    fix procinfo parse failing on thousands commas

--- src/procinfo.ts
+++ src/procinfo.ts
@@ -19,11 +19,16 @@ export function getProcessInfo(pid: number): ProcessInfo | null {
     if (!Number.isSafeInteger(pid) || pid < 0) throw new TypeError(`pid must be a safe positive integer, received: ${pid}`);
     if (os.platform() === 'win32') {
         const tasklist = chp.execSync(`tasklist /V /FO:CSV /FI "PID EQ ${pid}"`).toString('utf8');
-        const [k, v] = tasklist.split('\n').slice(0, 2).map(x => x.trim().split(','));
+        const [k, v] = tasklist.split('\n').slice(0, 2).map(row => {
+            const rowItems = row.trim().split('","');
+            rowItems[0] = rowItems[0]!.slice(1);
+            rowItems[rowItems.length - 1] = rowItems.at(-1)!.slice(0, -1);
+            return rowItems;
+        });
         if (!k || !v || !k.length || !v.length || k.length !== v.length) throw new Error(`Unrecognized tasklist output (ERR1): ${tasklist}`);
         if (k[0]!.startsWith('INFO:')) return null; // Process not found
         const info: ProcessInfo = {};
-        k.forEach((x, i) => info[x.slice(1, -1).replaceAll(' ', '').replace('#', '').toLowerCase()] = v[i]!.slice(1, -1));
+        k.forEach((x, i) => info[x.replaceAll(' ', '').replace('#', '').toLowerCase()] = v[i]);
         const npid = Number(info.pid);
         if (npid !== pid) throw new Error(`Unrecognized tasklist output (ERR2): ${tasklist}`);
         info.pid = npid;
